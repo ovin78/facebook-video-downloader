e2668034650477333c500dc87a1e8ed9a3f017a6
Deniz Dogan <deniz@dogan.se>
2016-01-28 19:30:03 +0100

    Change to tab indentation

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,22 +2,22 @@
 
 // inject the content script into all fully loaded facebook tabs
 chrome.tabs.query({
-  'status': 'complete'
+	'status': 'complete'
 }, function(tabs) {
-  tabs.forEach(function(tab) {
-    // check if it's a facebook tab
-    if (!tab.url.match(/^https?\:\/\/[^./]*\.facebook\.com/i))
-      return
+	tabs.forEach(function(tab) {
+		// check if it's a facebook tab
+		if (!tab.url.match(/^https?\:\/\/[^./]*\.facebook\.com/i))
+			return
 
-    // inject the content script
-    // then send the 'update' message to have it scan the page
-    chrome.tabs.executeScript(tab.id, { // inject JS
-      file: 'scripts/content.js',
-      runAt: 'document_end'
-    }, function() {
-      chrome.tabs.sendMessage(tab.id, { // tell to update
-        msg: 'update'
-      })
-    })
-  })
+		// inject the content script
+		// then send the 'update' message to have it scan the page
+		chrome.tabs.executeScript(tab.id, { // inject JS
+			file: 'scripts/content.js',
+			runAt: 'document_end'
+		}, function() {
+			chrome.tabs.sendMessage(tab.id, { // tell to update
+				msg: 'update'
+			})
+		})
+	})
 })
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -19,34 +19,34 @@ const FLOPPY = '\ud83d\udcbe'
 // extracts the HD and/or SD sources from a flashvars attribute value
 function extractSources(flashvars) {
 
-  // match `input` and `regexp` and replace all "\/" with just "/".
-  function extractURL(input, regexp) {
-    let m = input.match(regexp)
-    return m ? m[1].replace(SLASH_REGEXP, '/') : null
-  }
+	// match `input` and `regexp` and replace all "\/" with just "/".
+	function extractURL(input, regexp) {
+		let m = input.match(regexp)
+		return m ? m[1].replace(SLASH_REGEXP, '/') : null
+	}
 
-  // decode the URI encoded
-  let value = decodeURIComponent(flashvars)
-  let hd = extractURL(value, HD_REGEXP)
-  let sd = extractURL(value, SD_REGEXP)
+	// decode the URI encoded
+	let value = decodeURIComponent(flashvars)
+	let hd = extractURL(value, HD_REGEXP)
+	let sd = extractURL(value, SD_REGEXP)
 
-  return {
-    hd,
-    sd
-  }
+	return {
+		hd,
+		sd
+	}
 }
 
 // inserts the download buttons given the target node and the HD/SD sources
 function insertButtons(node, sources) {
-  if (!sources.hd && !sources.sd)
-    return
-  let html = '<p class="fbvd--wrapper">'
-  if (sources.hd)
-    html += `<a download target="_blank" href="${sources.hd}"><b>${FLOPPY}</b> HD</a>`
-  if (sources.sd)
-    html += `<a download target="_blank" href="${sources.sd}"><b>${FLOPPY}</b> SD</a>`
-  html += '</p>'
-  node.insertAdjacentHTML('beforebegin', html)
+	if (!sources.hd && !sources.sd)
+		return
+	let html = '<p class="fbvd--wrapper">'
+	if (sources.hd)
+		html += `<a download target="_blank" href="${sources.hd}"><b>${FLOPPY}</b> HD</a>`
+	if (sources.sd)
+		html += `<a download target="_blank" href="${sources.sd}"><b>${FLOPPY}</b> SD</a>`
+	html += '</p>'
+	node.insertAdjacentHTML('beforebegin', html)
 }
 
 // * find video source URLs for the given embed element
@@ -54,41 +54,41 @@ function insertButtons(node, sources) {
 // * if the embed element is a grandchild of a video element,
 //   insert the buttons  next to the video element instead
 function handleEmbed(node) {
-  node.dataset[DATA_ATTRIBUTE] = '1' // mark the node as handled
-  let flashvars = node.getAttribute('flashvars')
-  if (!flashvars)
-    return
-  let sources = extractSources(flashvars)
-  let grandParent = node.parentNode.parentNode
-  let target = grandParent.nodeName == 'VIDEO' ? grandParent : node
-  insertButtons(target, sources)
-  return node
+	node.dataset[DATA_ATTRIBUTE] = '1' // mark the node as handled
+	let flashvars = node.getAttribute('flashvars')
+	if (!flashvars)
+		return
+	let sources = extractSources(flashvars)
+	let grandParent = node.parentNode.parentNode
+	let target = grandParent.nodeName == 'VIDEO' ? grandParent : node
+	insertButtons(target, sources)
+	return node
 }
 
 // find all unhandled embed elements under `node`
 function handleDescendantsOf(node) {
-  let nodeList = node.querySelectorAll(`embed:not([data-${DATA_ATTRIBUTE}="1"])`)
-  let result = Array.from(nodeList, node => {
-    return handleEmbed(node)
-  })
+	let nodeList = node.querySelectorAll(`embed:not([data-${DATA_ATTRIBUTE}="1"])`)
+	let result = Array.from(nodeList, node => {
+		return handleEmbed(node)
+	})
 }
 
 // create an observer instance
 // find any embed elements below nodes that change
 let observer = new MutationObserver(mutations => {
-  mutations.forEach(rec =>
-    handleDescendantsOf(rec.target)
-  )
+	mutations.forEach(rec =>
+		handleDescendantsOf(rec.target)
+	)
 })
 
 // start observing immediately
 observer.observe(document, {
-  childList: true, // listen to changes to node children
-  subtree: true // listen to changes to descendants as well
+	childList: true, // listen to changes to node children
+	subtree: true // listen to changes to descendants as well
 })
 
 // update whenever the background scripts asks for it
 chrome.runtime.onMessage.addListener((request, sender, response) => {
-  if (request.msg == 'update')
-    handleDescendantsOf(document)
+	if (request.msg == 'update')
+		handleDescendantsOf(document)
 })
